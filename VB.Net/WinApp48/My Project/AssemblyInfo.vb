a807d15642728ecd8b0db57cfcf23ffb4e395ab9
Julio <86787811+JulioSilva123@users.noreply.github.com>
2023-08-02 22:01:40 -0300

    Código VB.Net

---
+++ VB.Net/WinApp48/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Coleção Hello World")>
+<Assembly: AssemblyDescription("Hello World VB.Net .NetFramework 4.8")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Julio Silva")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("Julio Silva")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("d439c694-15c8-4c7c-97a5-60e4f2755201")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
