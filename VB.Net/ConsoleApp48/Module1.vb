df34f1193a5ba739c8031edb6f79b5c158f26e73
Julio <86787811+JulioSilva123@users.noreply.github.com>
2023-08-02 21:56:58 -0300

    Código VB.Net

---
+++ VB.Net/ConsoleApp48/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello, World!")
+        Console.WriteLine("VB.Net - .Net Framework 4.8")
+        Console.WriteLine("")
+        Console.WriteLine("Pressione qualquer tecla para continuar...")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ VB.Net/ConsoleApp48/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Coleção Hello World")>
+<Assembly: AssemblyDescription("Hello World VB.Net .NetFramework 4.8")>
+<Assembly: AssemblyCompany("Julio Silva")>
+<Assembly: AssemblyProduct("ConsoleApp48")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("Julio Silva")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("bbc53386-b74f-4c63-8fac-eb176993494e")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
