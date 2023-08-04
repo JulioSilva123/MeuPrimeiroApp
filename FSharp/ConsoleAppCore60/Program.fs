ad92329feea2f9503113c19b4389855b51538623
Julio <86787811+JulioSilva123@users.noreply.github.com>
2023-08-04 00:43:34 -0300

    Código F#

---
+++ FSharp/ConsoleAppCore60/Program.fs
@@ -0,0 +1,7 @@
+﻿
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello, World!"
+printfn "\n"
+printfn "F# - .NetCore 6.0"
+printfn "Pressione qualquer tecla para continuar...\n"
